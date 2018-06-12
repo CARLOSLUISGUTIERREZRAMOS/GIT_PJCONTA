38dfd11f08e5ae54e4e5c2a2e3e343cf3909dbdb
Ricardo Jaramillo <rjaramillo@STARPERU.local>
2018-06-12 15:13:07 -0500

    avance

---
+++ PJCONTA/Helper/PasswordHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace PJCONTA.Helper
+{
+    public class PasswordHelper
+    {
+       
+    }
+}
